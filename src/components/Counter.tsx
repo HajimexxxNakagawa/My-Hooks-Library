11885126153229d84a75425bb5673b7954ae7ecd
Hajime Nakagawa <navyblue0914@icloud.com>
2021-03-07 21:09:43 +0900

    Add leverage to counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,13 +2,23 @@ import * as React from 'react'
 import { useCount } from '../hooks'
 
 const Counter: React.VFC = () => {
-  const [count, countUp, countDown] = useCount(1)
+  const [count, countUp, countDown] = useCount()
+  const [count10, countUp10, countDown10] = useCount(0, 10)
   return (
-    <div>
-      <p>{count}</p>
-      <button onClick={countDown}>-</button>
-      <button onClick={countUp}>+</button>
-    </div>
+    <>
+      <div>
+        <h1>1ずつ上昇</h1>
+        <p>{count}</p>
+        <button onClick={countDown}>-</button>
+        <button onClick={countUp}>+</button>
+      </div>
+      <div>
+        <h1>10ずつ上昇</h1>
+        <p>{count10}</p>
+        <button onClick={countDown10}>-</button>
+        <button onClick={countUp10}>+</button>
+      </div>
+    </>
   )
 }
 
--- src/hooks/useCount.ts
+++ src/hooks/useCount.ts
@@ -1,10 +1,13 @@
 import { useCallback, useState } from 'react'
 
 // カウントのみを扱うuseState Hook。カウントと、カウントアップ/ダウンの関数をそれぞれ返す。
-export function useCount(initialState = 0): [number, () => void, () => void] {
+export function useCount(
+  initialState = 0,
+  lev = 1
+): [number, () => void, () => void] {
   const [count, setcount] = useState(initialState)
-  const countUp = useCallback(() => setcount(count + 1), [count])
-  const countDown = useCallback(() => setcount(count - 1), [count])
+  const countUp = useCallback(() => setcount(count + lev), [count])
+  const countDown = useCallback(() => setcount(count - lev), [count])
 
   return [count, countUp, countDown]
 }
